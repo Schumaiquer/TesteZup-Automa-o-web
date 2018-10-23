27cbac3d567ec7d71b654cdec6a4cbb68acb49ab
Schumaiquer <schumaiquer08@gmail.com>
2018-10-23 15:02:32 -0300

    n

--- TesteCucumber01/src/main/java/runners/Runner.java
+++ TesteCucumber01/src/main/java/runners/Runner.java
@@ -5,9 +5,6 @@ import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
-
-
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
      //   features = "/Users/Ieza/Desktop/test/TesteZup-Automa-o-web/TesteCucumber01/src/main/resources/features",  //caminho das features
